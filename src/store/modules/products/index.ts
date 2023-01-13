4dd63ccedb28f05d013d9227f598ea8eb151526d
Mert Görkem Bozkurt <mgorkembozkurt@gmail.com>
2023-01-13 16:09:52 +0100

    products in json file, stringify & parse

--- src/store/modules/products/index.ts
+++ src/store/modules/products/index.ts
@@ -1,90 +1,15 @@
 import getters from './getters';
-// import mutations from './mutations';
-// import actions from './actions';
+import productsJSON from '../../../products.json'
 
 
 export default {
     namespaced: true,
     state() {
+      let result = JSON.stringify(productsJSON)
+
         return {
-            products:[
-                {
-                    code: 1,
-                    title: 'GARDENA combisystem-Vertikutier-Boy asdasdasd 1st',
-                    variant: 1,
-                    price: '44,99',
-                    oldPrice: '64,95',
-                    info: 'wenige',
-                    availability: 'medium',
-                    priceOffer: true,
-
-                  },
-                  {
-                    code: 2,
-                    title: 'Lorem asdqw ahashdasjd 2nd',
-                    variant: 0,
-                    price: '128,99',
-                    oldPrice: '222,95',
-                    info: 'mehrere',
-                    availability: 'medium',
-                    priceOffer: true,
-
-                  },
-                  {
-                    code: 3,
-                    title: 'Lorem asdqw ahashdasjd 3rd',
-                    variant: 3,
-                    price: '128,99',
-                    oldPrice: '322,95',
-                    info: 'mehrere',
-                    availability: 'high',
-                    priceOffer: false,
-                  },
-                  {
-                    code: 4,
-                    title:
-                      'GARDENA combisystem-Vertikutier-Boy asdasdasd GARDENA combisystem-Vertikutier-Boy asdasdasd 4th',
-                    variant: 0,
-                    price: '34,99',
-                    oldPrice: '74,95',
-                    info: 'wenige',
-                    availability: 'low',
-                    priceOffer: false,
-                  },
-                  {
-                    code: 5,
-                    title: 'GARDENA combisystem-Vertikutier-Boy asdasdasd 5th',
-                    variant: 2,
-                    price: '14,99',
-                    oldPrice: '94,95',
-                    info: 'wenige',
-                    availability: 'low',
-                    priceOffer: true,
-                  },
-                  {
-                    code: 6,
-                    title: 'GARDENA combisystem-Vertikutier-Boy asdasdasd 6th',
-                    variant: 5,
-                    price: '4,99',
-                    oldPrice: '44,95',
-                    info: 'wenige',
-                    availability: 'low',
-                    priceOffer: true,
-                  },
-                  {
-                    code: 7,
-                    title: 'GARDENA combisystem-Vertikutier-Boy asdasdasd 7th',
-                    variant: 0,
-                    price: '84,99',
-                    oldPrice: '94,95',
-                    info: 'wenige',
-                    availability: 'low',
-                    priceOffer: false,
-                  },
-            ]
+            products: JSON.parse(result)
         };
     },
     getters
-    // mutations,
-    // actions,
 };
--- src/stories/ProductList.stories.ts
+++ src/stories/ProductList.stories.ts
@@ -1,5 +1,6 @@
 import ProductList from './ProductList.vue';
 import { Meta, StoryFn } from '@storybook/vue3'
+import productsJSON from '../products.json'
 
 
 export default {
@@ -23,62 +24,10 @@ const Template: StoryFn<typeof ProductList> = (args) => ({
   });
 
   export const Examp = Template.bind({});
+  let result = JSON.stringify(productsJSON)
 
   Examp.args = {
-    products: [
-        {
-            code: 1,
-            title: 'GARDENA combisystem-Vertikutier-Boy asdasdasd 1st',
-            variant: 1,
-            price: '64,99',
-            oldPrice: '44,95',
-            info: 'wenige',
-            availability: 'medium',
-            priceOffer: true
 
-          },
-          {
-            code: 2,
-            title: 'Lorem asdqw ahashdasjd 2nd',
-            variant: 0,
-            price: '128,99',
-            oldPrice: '22,95',
-            info: 'mehrere',
-            availability: 'medium',
-            priceOffer: true
-          },
-          {
-            code: 3,
-            title: 'Lorem asdqw ahashdasjd 3rd',
-            variant: 2,
-            price: '128,99',
-            oldPrice: '22,95',
-            info: 'mehrere',
-            availability: 'high',
-            priceOffer: true
-
-          },
-          {
-            code: 4,
-            title:
-              'GARDENA combisystem-Vertikutier-Boy asdasdasd GARDENA combisystem-Vertikutier-Boy asdasdasd 4th',
-            variant: 3,
-            price: '64,99',
-            oldPrice: '44,95',
-            info: 'wenige',
-            availability: 'low',
-            priceOffer: true
-
-          },
-          {
-            code: 5,
-            title: 'GARDENA combisystem-Vertikutier-Boy asdasdasd 5th',
-            variant: 0,
-            price: '64,99',
-            oldPrice: '44,95',
-            info: 'wenige',
-            availability: 'low',
-            priceOffer: true
-          },
-    ]
+    products: JSON.parse(result)
+    
 };
